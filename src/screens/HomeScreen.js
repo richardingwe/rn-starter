2b4ebb1a22f5251a1fda339a9c2e237e0bae03c9
Richard Ingwe <richardingwe5@gmail.com>
2022-04-08 12:35:53 +0100

    navigation

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,15 +1,28 @@
 import React from 'react';
-import { Text, StyleSheet } from 'react-native';
+import { Text, StyleSheet, View, Button, TouchableOpacity } from 'react-native';
 
-const HomeScreen = () => {
-  return <Text style={styles.text}>Hello world</Text>;
+const HomeScreen = ({ navigation }) => {
+  return (
+    <View>
+      <Text style={styles.text}>Hello world</Text>
+      <Button
+        title="Go to Components demo"
+        onPress={() => navigation.navigate('Components')}
+      />
+      <TouchableOpacity onPress={() => navigation.navigate('List')}>
+        <Text>Go to List demo</Text>
+      </TouchableOpacity>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
   text: {
     fontSize: 30,
-    color: 'red',
-    backgroundColor: 'blue'
+    color: 'black',
+    // backgroundColor: 'lightblue',
+    textAlign: 'center',
+    paddingVertical: 16
   },
 });
 
